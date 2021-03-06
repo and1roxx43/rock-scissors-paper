9a5f9ed5a83bbde839a121f86b14599c6968a473
and1roxx43 <and1roxx43@gmail.com>
2021-03-07 00:00:27 +1100

    Added a reset button to index.html, add hover and active on buttons, add a function for the reset button in js file

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 ## Overview: A simple game of rock scissors paper. User will compete against the computer.
 ___
+- The game consist of 3 areas, a text box for user to enter a value, a play button to play, and a reset button to reset the game.
 - To play the game simply enter a value in the text box, and click play.
 - If no text is entered, user will be prompt to enter a value.
 - If wrong value is entered, user will be prompt to enter the correct value.
+- 
 ___
 ## URL for game
 
--- rock_paper_scissor.js
+++ rock_paper_scissor.js
@@ -1,5 +1,5 @@
 //Declaration of variables to store scores
- document.getElementById("btn_click").addEventListener("click", function() {
+ document.getElementById('btn_click').addEventListener('click', function() {
 
  
 let User = 0;
@@ -14,64 +14,71 @@ let values = ['R', 'P', 'S'];
 //let user = prompt("Enter either S, P or R");
 
 let user = document.getElementById('user').value;
-
+// if user does not enter a value, a message will be displayed to the user to enter a value.
   if(!user){
-    document.getElementById('userInputText').innerHTML = "Please enter the correct value to play!";
+    document.getElementById('userInputText').innerHTML = "Please enter the correct value to start playing!";
   return;
 }
 
 let userInput = user.toUpperCase();
 
+// if user enters the wrong value, a message will as user to enter the correct value.
 if((userInput !=='S') && (userInput !== 'P') && (userInput !== 'R'))
 {
   document.getElementById('userInputText').innerHTML = "Please enter a correct value!";
+
+  //this is to reset all text to blank
   document.getElementById('compInput').innerHTML = "";
   document.querySelector('.score').innerHTML = "";
   return;
 }
 else{
-//console log to see what user has entered.
-// console.log(`User has chosen ${userInput}`);
-document.getElementById('userText').innerHTML = "User has chosen " + userInput;
+//Display what user has entered in paragaph id userInput
+  document.getElementById('userText').innerHTML = "User has chosen " + userInput;
    
 /*---Computer input --- */
 //store a random value in variable index
 let index = Math.floor(Math.random() * values.length);
 //the return value is then stored in computer variable
 let computer = values[index];
-// console.log(`computer has chosen ${computer}`);
+
 document.getElementById('compInput').innerHTML = "Computer has chosen " + computer;
 
 //Run a condition on both variables to see if it's a draw, or computer wins, or user wins.
 if(userInput === computer){
-    // console.log("It's a draw!");
+  
     document.getElementById('userInputText').innerHTML = "It's a draw!";
-  //  return console.log(`Score is ${draw} draw`);
-  document.querySelector(".score").innerHTML = "No points!!";
+ 
+    document.querySelector(".score").innerHTML = "No points!!";
    
 } else if(
   (userInput === 'R' && computer === 'P') ||
   (userInput === 'P' && computer === 'S') ||
   (userInput === 'S' && computer === 'R'))
 {
-  // console.log("Computer wins!");
   document.getElementById('userInputText').innerHTML = "Computer wins!";
     compWin++;
-    // return console.log(`Score goes to computer by ${compWin} point`);
     document.querySelector(".score").innerHTML = "Score goes to Computer by " + compWin + " point";
 } else 
   {
-    // console.log("User wins!");
     document.getElementById('userInputText').innerHTML = "User wins!";
-    let userWinning = User++;
-    // return console.log(`Score goes to user by ${User} point`);    
+    let userWinning = User++;   
     document.querySelector(".score").innerHTML = "Score goes to user by " + User + " point";
   }
   } 
 }
 );
 
-//Run function to play
-// play();
+// Reset button to reset the game.
+document.getElementById('btn_click_reset').addEventListener('click', function(){
+
+  document.getElementById('userText').innerHTML = '';
+  document.getElementById('userInputText').innerHTML = '';
+  document.getElementById('compInput').innerHTML = ''
+  document.querySelector('.score').innerHTML = '';
+
+});
+
+
 
 
